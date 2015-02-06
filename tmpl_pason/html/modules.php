21103c2fc920e47108a216b8e561e8daea3ff12b
Chad Windnagle <drmmr763@gmail.com>
2015-02-06 15:02:33 -0500

    add module chrome for parallax

--- tmpl_pason/html/modules.php
+++ tmpl_pason/html/modules.php
@@ -13,3 +13,39 @@ function modChrome_slide($module, $params, $attribs)
     echo        $module->content;
     echo    '</div>';
 }
+
+function modChrome_featured($module, $params, $attribs)
+{
+    $title      = trim($module->title);
+    $titleParts = explode(' ', $title);
+
+    if (is_array($titleParts))
+    {
+        $title = '';
+
+        $count = 1;
+        foreach ($titleParts as $part)
+        {
+            $title .= $part . ' ';
+
+            if (($count % 2) == 0)
+            {
+                $title .= '<br />';
+            }
+            $count++;
+        }
+    }
+
+
+    echo '<div class="' . $params->get('moduleclass_sfx') . '">';
+    echo    '<div class="featured-solution-inner">';
+    echo        '<div class="featured-solution-title">';
+    echo            '<h5>' . $title . '</h5>';
+    echo            '<img src="/images/down-arrow.png" />';
+    echo        '</div>';
+    echo        '<div class="featured-solution-content">';
+    echo            $module->content;
+    echo        '</div>';
+    echo    '</div>';
+    echo '</div>';
+}
