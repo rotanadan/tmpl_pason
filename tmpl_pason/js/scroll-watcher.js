bec5a2742878ff696bfddf9504173af9013202b2
Chad Windnagle <drmmr763@gmail.com>
2015-03-02 00:00:04 -0500

    update comenting. no changes

--- tmpl_pason/js/scroll-watcher.js
+++ tmpl_pason/js/scroll-watcher.js
@@ -11,6 +11,7 @@ jQuery(document).ready(function($){
      */
     function makeScroll(element, direction)
     {
+        // our ruler is our element
         var rule = element;
 
         var ruler_css = rule.css('backgroundPosition');
@@ -18,23 +19,26 @@ jQuery(document).ready(function($){
         // position comes in as '0% 0%'
         var ruler_xy = ruler_css.split(" ");
 
+        var ruler_y  = parseInt(ruler_xy[1]); // currently unused
         var ruler_x  = parseInt(ruler_xy[0]);
 
+        // check if we are going left or right
         if (direction == 'right')
         {
+            // right we add
             ruler_x = ruler_x + 1;
         }
 
         if (direction == 'left')
         {
+            // left we subtract
             ruler_x = ruler_x - 1;
         }
 
-        // currently unused
-        var ruler_y  = parseInt(ruler_xy[1]);
-
+        // build the position property
         var position = ruler_x + 'px ' + '0px';
 
+        // reset the css
         rule.css('background-position', position );
     }
 
